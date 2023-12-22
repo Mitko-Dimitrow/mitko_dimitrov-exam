98b49b1212cdc0fcc682b8a48f95ba1aad2b2006
Mitko-Dimitrow <mitimitko@abv.bg>
2023-12-22 11:06:14 +0200

    URL fix

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Add-Grade');
+    await page.goto('https://mitko-dimitrov-exam.onrender.com/Add-Grade');
     const form = await page.$('input');
     expect(form).toBeTruthy();
   });
--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/My-Grades');
+    await page.goto('https://mitko-dimitrov-exam.onrender.com/My-Grades');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
